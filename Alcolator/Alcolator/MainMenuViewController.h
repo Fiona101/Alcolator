9c11c0923496becee87b132a8451d378d7f01927
Fiona Alpe <fiona.alpe@gmail.com>
2016-05-27 18:29:21 +0300

    Checkpoint 21 successfully completed

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Fiona Alpe on 5/27/16.
+//  Copyright © 2016 Fiona Alpe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
---
+++ Alcolator/Alcolator/MainMenuViewController.m
@@ -0,0 +1,20 @@
+//
+//  MainMenuViewController.m
+//  Alcolator
+//
+//  Created by Fiona Alpe on 5/27/16.
+//  Copyright © 2016 Fiona Alpe. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+
+
+
+@end
